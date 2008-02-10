3652362a230e45ba47ae461a21ce5dc7d6496c50
Dave Peterson <pete2786@umn.edu>
2008-02-10 20:09:14 +0000

    added my controllers as well as blank specs for said controllers.

---
+++ spec/controllers/invitations_controller_spec.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe InvitationsController do
+
+  #Delete this example and add some real ones
+  it "should use InvitationsController" do
+    controller.should be_an_instance_of(InvitationsController)
+  end
+
+end
