fe88c9a0197ec912f65ce22762ae0fc7c2b99173
Dave Peterson <pete2786@umn.edu>
2008-02-11 00:03:03 +0000

    ... whoops

--- spec/controllers/invitations_controller_spec.rb
+++ spec/controllers/invitations_controller_spec.rb
@@ -1,10 +1,89 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe InvitationsController do
+describe InvitationsController, "#route_for" do
 
-  #Delete this example and add some real ones
-  it "should use InvitationsController" do
-    controller.should be_an_instance_of(InvitationsController)
+  it "should map { :controller => 'invitations', :action => 'index' } to /invitations" do
+    #route_for(:controller => "invitations", :action => "index").should == "/invitations"
   end
 
+  it "should map { :controller => 'invitations', :action => 'new' } to /invitations/new" do
+    #route_for(:controller => "invitations", :action => "new").should == "/invitations/new"
+  end
+
+  it "should map { :controller => 'invitations', :action => 'show', :id => 1 } to /invitations/1" do
+    #route_for(:controller => "invitations", :action => "show", :id => 1).should == "/invitations/1"
+  end
+
+  it "should map { :controller => 'invitations', :action => 'edit', :id => 1 } to /invitations/1;edit" do
+    #route_for(:controller => "invitations", :action => "edit", :id => 1).should == "/invitations/1;edit"
+  end
+
+  it "should map { :controller => 'invitations', :action => 'update', :id => 1} to /invitations/1" do
+    #route_for(:controller => "invitations", :action => "update", :id => 1).should == "/invitations/1"
+  end
+
+  it "should map { :controller => 'invitations', :action => 'destroy', :id => 1} to /invitations/1" do
+    #route_for(:controller => "invitations", :action => "destroy", :id => 1).should == "/invitations/1"
+  end
+end
+
+describe InvitationsController, " handling GET /invitations" do
+  it "should be successful"
+  it "should render index template"
+  it "should find all weathers"
+  it "should assign the found weathers for the view"
+end
+
+describe InvitationsController, " handling GET /invitations.xml" do
+  it "should be successful"
+  it "should find all invitations"
+  it "should render the found invitations as XML"
+end
+
+describe InvitationsController, " handling GET /invitations/1" do
+  it "should be successful"
+  it "should find the invitation requested"
+  it "should render the show template"
+  it "should assigned the found invitation to the view"
+end
+
+describe InvitationsController, " handling GET /invitations/1.xml" do
+  it "should be successful"
+  it "should find the invitation requested"
+  it "should render the found invitation as XML"
+end
+
+describe InvitationsController, " handling GET /invitations/new" do
+  it "should be successful"
+  it "should render new template"
+  it "should create a new invitation"
+  it "should not save the new invitation"
+  it "should assign the new invitation to the view"
+end
+
+describe InvitationsController, " handling GET /invitations/1/edit" do
+  it "should be successful"
+  it "should render the edit template"
+  it "should find the invitation requested"
+  it "should assign the found invitation to the view"
+end
+
+describe InvitationController, " handling POST /invitations" do
+  it "should create a new invitation by type"
+  it "should have a survey_id is type is 'Survey'"
+  it "should have a network_id if the type is 'Network'"
+  it "should redirect to the new invitation"
+end
+
+describe InvitationsController, " handling PUT /invitations/1" do
+  it "should find the invitation requested"
+  it "should update the selected invitation"
+  it "should assign the found invitation to the view"
+  it "should redirect to invitations default view"
+end
+
+describe InvitationsController, " handling DELETE /invitations/1" do
+  it "should find the invitation requested"
+  it "should destory the invitation requested"
+  it "should redirect to the dashboard"
 end
--- spec/controllers/surveys_controller_spec.rb
+++ spec/controllers/surveys_controller_spec.rb
@@ -1,10 +1,91 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe SurveysController do
+  describe SurveysController, "#route_for" do
 
-  #Delete this example and add some real ones
-  it "should use SurveysController" do
-    controller.should be_an_instance_of(SurveysController)
+    it "should map { :controller => 'surveys', :action => 'index' } to /surveys" do
+      #route_for(:controller => "surveys", :action => "index").should == "/surveys"
+    end
+
+    it "should map { :controller => 'surveys', :action => 'new' } to /surveys/new" do
+      #route_for(:controller => "surveys", :action => "new").should == "/surveys/new"
+    end
+
+    it "should map { :controller => 'surveys', :action => 'show', :id => 1 } to /surveys/1" do
+      #route_for(:controller => "surveys", :action => "show", :id => 1).should == "/surveys/1"
+    end
+
+    it "should map { :controller => 'surveys', :action => 'edit', :id => 1 } to /surveys/1;edit" do
+      #route_for(:controller => "surveys", :action => "edit", :id => 1).should == "/surveys/1;edit"
+    end
+
+    it "should map { :controller => 'surveys', :action => 'update', :id => 1} to /surveys/1" do
+      #route_for(:controller => "surveys", :action => "update", :id => 1).should == "/surveys/1"
+    end
+
+    it "should map { :controller => 'surveys', :action => 'destroy', :id => 1} to /surveys/1" do
+      #route_for(:controller => "surveys", :action => "destroy", :id => 1).should == "/surveys/1"
+    end
   end
 
+  describe SurveysController, " handling GET /surveys" do
+    it "should be successful"
+    it "should render index template"
+    it "should find all weathers"
+    it "should assign the found weathers for the view"
+  end
+
+  describe SurveysController, " handling GET /surveys.xml" do
+    it "should be successful"
+    it "should find all surveys"
+    it "should render the found surveys as XML"
+  end
+
+  describe SurveysController, " handling GET /surveys/1" do
+    it "should be successful"
+    it "should find the survey requested"
+    it "should render the show template"
+    it "should assigned the found survey to the view"
+  end
+
+  describe SurveysController, " handling GET /surveys/1.xml" do
+    it "should be successful"
+    it "should find the survey requested"
+    it "should render the found survey as XML"
+  end
+
+  describe SurveysController, " handling GET /surveys/new" do
+    it "should be successful"
+    it "should render new template"
+    it "should create a new survey"
+    it "should not save the new survey"
+    it "should assign the new survey to the view"
+  end
+
+  describe SurveysController, " handling GET /surveys/1/edit" do
+    it "should be successful"
+    it "should render the edit template"
+    it "should find the survey requested"
+    it "should assign the found survey to the view"
+  end
+
+  describe SurveyController, " handling POST /surveys" do
+    it "should create a new survey by sponsor_id"
+    it "should redirect to the new survey"
+  end
+
+  describe SurveysController, " handling PUT /surveys/1" do
+    it "should find the survey requested"
+    it "should update the selected survey"
+    it "should assign the found survey to the view"
+    it "should redirect to surveys default view"
+  end
+
+  describe SurveysController, " handling DELETE /surveys/1" do
+    it "should find the survey requested"
+    it "should destory the survey requested"
+    it "should delete related questions"
+    it "should delete related discussions"
+    it "should delete related invitations"
+    it "should redirect to the dashboard"
+  end
 end
