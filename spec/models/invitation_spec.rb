e447fa8979d771ec2375a957cf44063dec38b03f
Josh Fleck <flec0025@umn.edu>
2008-02-12 01:36:32 +0000

    Moved network_invitation and survey_invitation model specs into invitation model spec

--- spec/models/invitation_spec.rb
+++ spec/models/invitation_spec.rb
@@ -1,10 +1,124 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe Invitation do
-  it "should have a type"
-  it "should not allow an invalid type"
-  it "should belong to an invitee"
-  it "should belong to an inviter"
-  it "should be able to be fetched be invitee_id"
-  it "should be able to be fetched be inviter_id"
+module InvitationSpecHelper
+
+  def valid_invitation_attributes
+    {
+      
+    }
+  end
 end
+
+describe Invitation, "that does not exist", :shared => true do
+
+  it "should belong to an invitee" do
+  #future code here pending review
+  end
+  
+  it "should belong to an inviter" do
+  #future code here pending review
+  end
+  
+end
+
+describe Invitation, "that does exist", :shared => true do
+   
+  include InvitationSpecHelper
+
+  it "is invalid without an invitee" do
+  #future code here pending review
+  end
+  
+  it "is invalid without an inviter" do
+  #future code here pending review
+  end
+  
+end
+
+module NetworkInvitationSpecHelper
+
+  def valid_network_invitation_attributes
+    {
+      
+    }
+  end
+end
+
+describe NetworkInvitation, "that does not exist" do
+
+  it_should_behave_like "Invitation that does not exist"  
+
+  it "belongs to a network" do
+  #future code here pending review
+  end
+   
+  it "inherits from invitation" do
+  #network_invite.class.superclass.name.should == "Invite"
+  #future code here pending review
+  end    
+ 
+end
+
+describe NetworkInvitation, "that does exist" do
+   
+  include NetworkInvitationSpecHelper
+
+  before(:each) do
+    @network_invitation = NetworkInvitation.new
+  end
+
+  it_should_behave_like "Invitation that does exist" 
+  
+  it "should be valid on create" do
+  #future code here pending review
+  end  
+    
+  it "should be invalid if a network is not specified" do
+  #future code here pending review
+  end  
+  
+ end 
+ 
+module SurveyInvitationSpecHelper
+
+  def valid_survey_invitation_attributes
+    {
+      
+    }
+  end
+end
+
+describe SurveyInvitation, "that does not exist" do
+
+  it_should_behave_like "Invitation that does not exist"  
+
+  it "inherits from invitation" do
+  #survey_invite.class.superclass.name.should == "Invite"
+  #future code here pending review
+  end    
+ 
+  it "should belong to a survey" do
+  #future code here pending review  
+  end
+  
+end
+
+describe SurveyInvitation, "that does exist" do
+   
+  include SurveyInvitationSpecHelper
+
+  before(:each) do
+    @survey_invitation = SurveyInvitation.new
+  end
+
+  it_should_behave_like "Invitation that does exist" 
+      
+  it "should be valid on create" do
+  #future code here pending review
+  end  
+ 
+  it "should be invalid if a survey is not specified" do
+    #future code here pending review
+  end
+  
+end 
--- spec/models/survey_invitation_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe SurveyInvitation do
-  it "should belong to a survey"
-  it "should require a survey_id"
-  it "should have the type 'Survey'"
-  it "can be fetched by survey_id"
-end
