42db42545266eac2562fd245482c1dd105be869a
Josh Fleck <flec0025@umn.edu>
2008-02-12 01:37:36 +0000

    Added some validators

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,4 +1,8 @@
 class Invitation < ActiveRecord::Base
   belongs_to :invitee, :class_name => "Organization", :foreign_key => "invitee_id"
   belongs_to :inviter, :class_name => "Organization", :foreign_key => "inviter_id"
+  
+  validates_presence_of :invitee
+  validates_presence_of :inviter
+  
 end
--- app/models/survey_invitation.rb
+++ app/models/survey_invitation.rb
@@ -1,3 +1,7 @@
 class SurveyInvitation < Invitation
+
   belongs_to :survey
+  
+  validates_presence_of :survey
+  
 end
