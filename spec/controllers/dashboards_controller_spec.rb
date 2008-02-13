b6fd32ea173a7aa73e7274a42451caa4d561d76c
Josh Fleck <flec0025@umn.edu>
2008-02-13 01:52:00 +0000

    Added some more specs here.

--- spec/controllers/dashboards_controller_spec.rb
+++ spec/controllers/dashboards_controller_spec.rb
@@ -1,7 +1,15 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe DashboardsController do
-  
+describe DashboardsController, "#route_for" do
+
+  it "should map { :controller => 'dashboards', :action => 'index' } to /dashboards" do
+    #route_for(:controller => "dashboards", :action => "index").should == "/dashboards"
+  end
+
+end
+ 
+describe DashboardsController, " handling GET /dashboards" do
+
   describe "GET 'show'" do
     it "should be successful" do
       get 'show'
@@ -9,4 +17,14 @@ describe DashboardsController do
     end
   end
   
+  it "should render index template"
+  it "should find all received messages since last login"
+  it "should find all survey invitations received since last login"
+  it "should find all network invitations received since last login"
+  it "should find all completed surveys for your org since last login"
+  it "should assign the found information for the view"
 end
+
+  
+
+  
