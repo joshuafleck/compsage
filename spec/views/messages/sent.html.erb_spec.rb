8415c01ebf3b9fece6bef44d5b8be80f874f5440
Josh Fleck <flec0025@umn.edu>
2008-02-16 20:35:22 +0000

    forgot to add the sent messages view

---
+++ spec/views/messages/sent.html.erb_spec.rb
@@ -0,0 +1,11 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "/messages/sent" do
+
+  before(:each) do
+    render 'messages/sent'
+  end
+  
+  it "should render the messages list"
+
+end
