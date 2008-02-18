f5c0b0ba84a273a6cf91bbf73da807c7aee1b6c4
Josh Fleck <flec0025@umn.edu>
2008-02-18 20:31:31 +0000

    updated account specs

--- spec/views/accounts/index.html.erb_spec.rb
+++ spec/views/accounts/index.html.erb_spec.rb
@@ -6,7 +6,12 @@ describe "/accounts/index" do
     render 'accounts/index'
   end
   
-  it "should render the account attributes"
+  it "should display the email address"
+  it "should display the public/private status"  
+  it "should display the location"
+  it "should display the city"
+  it "should display the state"
+  it "should display the name"
   it "should have an edit account link"  
 
 end
