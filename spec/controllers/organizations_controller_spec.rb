b4aaf883bbbbadae8d04c16f6c879489f0f1e5da
Brian Terlson <brian.terlson@gmail.com>
2008-02-24 05:23:48 +0000

    Removed crap from organization controller spec.

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -19,48 +19,6 @@ describe OrganizationsController, "#route_for" do
   
 end
 
-describe OrganizationsController, " handling sign up stuff" do
-  fixtures :organizations
-
-  it 'allows signup' do
-    lambda do
-      create_organization
-      response.should be_redirect      
-    end.should change(Organization, :count).by(1)
-  end
-
-  it 'requires password on signup' do
-    lambda do
-      create_organization(:password => nil)
-      assigns[:organization].errors.on(:password).should_not be_nil
-      response.should be_success
-    end.should_not change(Organization, :count)
-  end
-  
-  it 'requires password confirmation on signup' do
-    lambda do
-      create_organization(:password_confirmation => nil)
-      assigns[:organization].errors.on(:password_confirmation).should_not be_nil
-      response.should be_success
-    end.should_not change(Organization, :count)
-  end
-
-  it 'requires email on signup' do
-    lambda do
-      create_organization(:email => nil)
-      assigns[:organization].errors.on(:email).should_not be_nil
-      response.should be_success
-    end.should_not change(Organization, :count)
-  end
-  
-  it "should redirect to dashboard"
-  
-  def create_organization(options = {})
-    post :create, :organization => { :email => 'quire@example.com',
-      :password => 'quire', :password_confirmation => 'quire' }.merge(options)
-  end
-end
-
 describe OrganizationsController, "handling GET /organizations/1" do
   it "should be successful"
   it "should render show template"
