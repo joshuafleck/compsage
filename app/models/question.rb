a910ddf458f4c7818682a14a896d33bf2eb2e634
Dave Peterson <pete2786@umn.edu>
2008-04-06 02:48:42 +0000

    question spec so far... need discussion on handling response specs.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,8 +1,64 @@
 class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :responses, :dependent => :delete_all
-  
   acts_as_list :scope => :survey_id
   
   serialize :options
+  
+  validates_presence_of :survey
+  validates_presence_of :question_type
+  validates_presence_of :options, :message => "You must include some options", :if => Proc.new { |question| question.has_options? }
+  validates_length_of :text, :within => 1..1000, :message => "A question is required to have associated text."
+  
+  
+  QUESTION_TYPES = [  ["Single-line Text Box", 'text_field'],
+                      ["Numerical Input", 'numerical_field'],
+                      ["Multi-line Text Box", 'text_area'],
+                      ["Multiple Choice - 1 Answer", 'radio'],
+                      ["Multiple Choice - Any Answer", 'check'],
+                      ["Textual Comments or Instructions", 'text']
+                    ]
+  TYPES_WITH_OPTIONS = ['radio', 'check']
+  NUMERICAL_RESPONSES = { 'text_field' => false,
+                                'text_area' => false,
+                                'numerical_field' => true,
+                                'radio' => true,
+                                'check' => true,
+                                'text' => false
+                        }
+  DEFAULT_HTML_ATTRIBUTES = { 'text_field' => {:size => 30},
+                              'numerical_field' => {:size => 6},
+                              'text_area' => {  :rows => 3,
+                                                :cols => 40},
+                              'radio' => {},
+                              'check' => {},
+                              'text' => {}
+                            }
+  DEFAULT_QUESTION_ATTRIBUTES = { 'text_field' => {},
+                                  'numerical_field' => {},
+                                  'text_area' => {:style => 'vertical'},
+                                  'radio' => {},
+                                  'check' => {},
+                                  'text' => {}
+                                }
+  DEFAULT_OPTIONS = {
+    'Yes/No' => ['Yes', 'No'],
+    'True/False' => ['True', 'False'],
+    '5 Point Agreement' => ['Strongly Agree', 'Agree', 'Neutral', 'Disagree', 'Strongly Disagree'],
+    'Extent Observed' => ['Always Observe', 'Often Observe', 'Sometimes Observe', 'Seldom Observe', 'Never Observe'],
+    '7 Point Agreement' => ['7 - Highest Agreement', '6', '5', '4', '3','2','1 - Lowest Agreement']
+  }
+  
+  def answerable?()
+    return !(self[:question_type] == 'text')
+  end
+  
+  def has_options?()
+    return TYPES_WITH_OPTIONS.include?(self[:question_type])
+  end
+  
+  def numerical_response?()
+    return NUMERICAL_RESPONSES[self[:question_type]]
+  end
+  
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,45 +1,109 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+module QuestionSpecHelper
+
+  def valid_question_attributes
+    {
+      :text => "What is the meaning of life?",
+      :position => 1,
+      :question_type => "radio",
+      :question_parameters => {},
+      :html_parameters => {},
+      :options => ['Yes', 'No'],
+      :responses => [],
+      :survey => mock_model(Survey)
+    }
+  end
+  
+end
+
 describe Question do
+  include QuestionSpecHelper
+  
   before(:each) do
     @question = Question.new
   end
   
-  it "should be valid"
+  it "should be valid" do
+    @question.attributes = valid_question_attributes
+    @question.should be_valid
+  end
   
-  it "should be invalid without a survey"
+  it "should be invalid without a survey" do
+    @question.attributes = valid_question_attributes.except(:survey)
+    @question.should have(1).error_on(:survey)
+  end
   
-  it "should have many responses"
+  it "should have many responses" do
+    Question.reflect_on_association(:responses).should_not be_nil
+  end
   
-  it "should require question text"
+  it "should require question text" do
+    @question.attributes = valid_question_attributes.except(:text)
+    @question.should have(1).error_on(:text)
+  end
   
-  it "should be invalid with question text longer than 1000 characters"
+  it "should be invalid with question text longer than 1000 characters" do
+    @question.attributes = valid_question_attributes.with(:text => "a" * 1001)
+    @question.should have(1).error_on(:text)
+  end
 end
 
 describe Question, "with options", :shared => true do
+  include QuestionSpecHelper
   
-  it "should be invalid without some options"
+  before(:each) do
+    @question = Question.new
+    @question.attributes = valid_question_attributes.with(:question_type => 'check')
+  end
   
-  it "should save a numerical response"
+  it "should be invalid without some options" do
+    @question.attributes = @question.attributes.with(:options => [])
+    @question.should have(1).error_on(:options)
+  end
   
-  it "should be answerable"
+  it "should save a numerical response" do
+    @question.numerical_response?.should be_true
+  end
+  
+  it "should be answerable" do
+    @question.answerable?.should be_true
+  end
 end
 
 describe Question, "without options with a textual response", :shared => true do
+  include QuestionSpecHelper
   
-  it "should save a textual response"
+  before(:each) do
+    @question = Question.new
+    @question.attributes = valid_question_attributes.with(:question_type => 'text_area')
+  end  
+  it "should save a textual response" do
+    @question.numerical_response?.should be_false
+  end
   
-  it "should be answerable"
+  it "should be answerable" do
+    @question.answerable?.should be_true
+  end
 
 end
 
 describe Question, "without options with a numerical response", :shared => true do
+  include QuestionSpecHelper
   
-  it "should save a numerical response"
+  before(:each) do
+    @question = Question.new
+    @question.attributes = valid_question_attributes.with(:question_type => 'numerical_field')
+  end
+  it "should save a numerical response" do
+    @question.numerical_response?.should be_true
+  end
 
-  it "should be invalid if it's not a number"
+  it "should be invalid if it's not a number" #should this be handled by response model?
   
-  it "should be answerable"
+  it "should be answerable" do
+    @question.answerable?.should be_true
+  end
   
 end
 
@@ -86,7 +150,14 @@ end
 
 
 describe Question, "that is just plain old text (like instructions)" do
+  include QuestionSpecHelper
   
-  it "should not be answerable"
+  before(:each) do
+    @question = Question.new
+    @question.attributes = valid_question_attributes.with(:question_type => 'text')
+  end
+  it "should not be answerable" do
+    @question.answerable?.should be_false
+  end
   
 end
