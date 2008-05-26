0c6fc31683b0342bb5ebcc69c18cab08d73fd97f
Brian Terlson <brian.terlson@gmail.com>
2008-05-26 23:26:04 +0000

    Forgot the observer model from earlier.

---
+++ app/models/invitation_observer.rb
@@ -0,0 +1,14 @@
+class InvitationObserver < ActiveRecord::Observer
+  
+  def after_save(invitation)
+    # send emails
+    if invitation.is_a?(ExternalNetworkInvitation)
+      Notifier.deliver_external_network_invitation_notification(invitation)
+    elsif invitation.is_a?(NetworkInvitation)
+      Notifier.deliver_network_invitation_notification(invitation)
+    end
+    
+    # TODO: Survey Invitation stuff.
+  end
+  
+end
