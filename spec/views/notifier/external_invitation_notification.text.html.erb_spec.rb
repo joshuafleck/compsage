4fcbd4ef167b57aef3198b9017b48849526a5706
Josh Fleck <flec0025@umn.edu>
2008-06-09 01:39:11 +0000

    Adding specs for external invitations email views

---
+++ spec/views/notifier/external_invitation_notification.text.html.erb_spec.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe "external invitation email" do
+  
+  it "should name the invitee"
+  it "should name the sender of the invitation"
+  it "should name the sender of the invitation's organization"
+  it "should display the sender of the invitation's message"
+  it "should link to the new account page"
+  it "should have an invitation key"
+
+end
