bae784604bf031aef5b68850e940f27230558365
Josh Fleck <josh@331university.dyndns.org>
2008-06-14 15:58:33 -0500

    testing a small change to the abuse email view

--- spec/views/notifier/abuse_report_notification.text.plain.erb_spec.rb
+++ spec/views/notifier/abuse_report_notification.text.plain.erb_spec.rb
@@ -1,8 +1,19 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe "report abuse email" do
+
+  before(:each) do
+    @offending_organization = mock_model(Organization)
+  end
   
-  it "should contain a discussion id"
+  def render_view
+    render 'notifier/abuse_report_notification.text.plain.erb'  
+  end
+  
+  it "should contain a discussion id" do
+    render_view
+    puts response.body
+  end
   it "should name the organization responsible"
   it "should contain the discussion attributes"
 
