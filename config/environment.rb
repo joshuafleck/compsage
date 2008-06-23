4278e0f4039aa0c80f151e817cffefa6a8b8fc94
Brian Terlson <brian.terlson@gmail.com>
2008-06-22 20:27:37 -0500

    Some additional mailer tweaks.
    * Updated action_mailer to have a default host and port.  These are defined in the environment.rb file in the config directory.
    * Updated the mailers to use named routes and specify that the host should be appended to the URL.
    * Added a mailer for network invitations.

--- config/environment.rb
+++ config/environment.rb
@@ -9,4 +9,6 @@ require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
   config.action_controller.session = { :session_key => "_myapp_session", :secret => "535b541d23b19ce0e7a1090c403771d6" }
   config.active_record.observers = :invitation_observer
+  
+  config.action_mailer.default_url_options = {:host => "localhost", :port => 3000}
 end
