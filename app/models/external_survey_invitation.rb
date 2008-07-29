22767fb28a3aa1092ae01096e30e993093df4f0c
Brian Terlson <brian.terlson@gmail.com>
2008-07-29 16:16:00 -0500

    External Survey Invitations have many responses now so interface is identical to organizations.  There may be a better way to do this...

--- app/models/external_survey_invitation.rb
+++ app/models/external_survey_invitation.rb
@@ -2,12 +2,11 @@ class ExternalSurveyInvitation < ExternalInvitation
   belongs_to :survey
   
   has_many :discussions, :as => :responder
-  has_one :participation, :as => :participant
+  has_many :participations, :as => :participant
   has_many :responses, :through => :participations
   
   validates_presence_of :survey
   validates_presence_of :name
   
   attr_accessible :survey, :disccusions, :responses
-  
 end
