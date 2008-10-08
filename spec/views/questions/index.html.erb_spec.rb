be985380df8bad577c2b522dd86a64db17bdb347
Brian Terlson <brian.terlson@gmail.com>
2008-10-07 22:12:52 -0500

    These are worthless with cucumber in the pipe.  So long hours and hours of work.

--- spec/views/questions/index.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.dirname(__FILE__) + '/../../spec_helper'
-
-describe "/questions/index" do
-  it "should have a link to continue"
-  it "should have a link to cancel"
-  it "should show the survey title"
-  it "should show a list of questions w/responses"
-end
