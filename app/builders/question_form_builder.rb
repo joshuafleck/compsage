fc5936ac9e0fb8186fc874ca0ea98efafa888e92
Brian Terlson <brian.terlson@gmail.com>
2008-10-29 14:00:06 -0500

    multiple response handling.  Adding this so it's here if we need it later, but now I'm changing how checkboxes work because this code is way too crufty.

--- app/builders/question_form_builder.rb
+++ app/builders/question_form_builder.rb
@@ -1,5 +1,7 @@
 require 'enumerator'
 class QuestionFormBuilder < ActionView::Helpers::FormBuilder
+  
+  # builds  a form field for a survey question.
   def form_field
     case question.question_type
     when "text_field"
@@ -11,8 +13,44 @@ class QuestionFormBuilder < ActionView::Helpers::FormBuilder
       question.options.to_enum(:each_with_index).collect { |option, index|
         @template.content_tag(:div, radio_button(:numerical_response, index) + " " + option)
       }.join("")
-    end +
-    " Qualifications: " + text_field(:qualifications)
+    when "textarea"
+      label(:textual_response, question.text) + text_area(:textual_response, :rows => 5, :cols => 50)
+    when "checkbox"
+      p object.inspect
+      @template.content_tag(:label, question.text) +
+      question.options.to_enum(:each_with_index).collect { |option, index|
+        @template.content_tag(:div,
+          @template.check_box_tag(
+            "#{@object_name}[#{@options[:index]}][numerical_response][]",
+            index,
+            checked_option?(index),
+            :id => "#{@object_name.sub('[', '_').chop}_#{@options[:index]}_numerical_response_#{index}") + " #{option}"
+        )
+      }.join("")
+    end
+  end
+  
+  # Need to override fields_for to change it's behavior when the object is an array.
+  # We need it to treat it as fields for the first array element and assign the object to be the
+  # array.
+  
+  def fields_for(record_or_name_or_array, *args, &block)
+    p "Building feilds for #{record_or_name_or_array.inspect}"
+    case record_or_name_or_array
+    when String, Symbol
+      name = "#{object_name}[#{record_or_name_or_array}]"
+    when Array
+      p "Is array.  Setting object to be the array."
+      object = record_or_name_or_array
+      name = "#{object_name}[#{ActionController::RecordIdentifier.singular_class_name(object.first)}]"
+      args.unshift(object)
+    else
+      object = record_or_name_or_array
+      name = "#{object_name}[#{ActionController::RecordIdentifier.singular_class_name(object)}]"
+      args.unshift(object)
+    end
+
+    @template.fields_for(name, *args, &block)
   end
   
   private
@@ -33,5 +71,12 @@ class QuestionFormBuilder < ActionView::Helpers::FormBuilder
     else
       ""
     end
+  end
+  
+  # checks for whether the response(s) include one where the specified option was checked.
+  def checked_option?(option)
+    responses = [*object]
+    
+    !!responses.find{|r| r.numerical_response == option}
   end
 end
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -2,7 +2,7 @@ class Participation < ActiveRecord::Base
   belongs_to :participant, :polymorphic => true
   belongs_to :survey
   
-  has_many :responses
+  has_many :responses, :dependent => :destroy
   
   validates_presence_of :participant, :survey
   validates_associated :responses
@@ -16,16 +16,55 @@ class Participation < ActiveRecord::Base
       'Organization', 
       'Organization']  
   
-  # sets up the response for this participation.
+  # Sets up the response for this participation from the form parameters.
+  #
+  # Note: Checkboxes are handled by creating multiple responses to the same question.  This allows
+  # for easy generation of statistics straight from the DB, but has some pretty poor code side
+  # effects.  For example, the respondant's qualifications are stored with whatever the first
+  # response is, ordered by option choice.  This is a little finicky, but works for now.
+  
   def response=(question_response_params)
-    new_responses = []
-    question_response_params.each do |question_id, attributes|
-      next if attributes.values.join.empty?
-      question_response = response[question_id] || responses.new(:question_id => question_id)
-      question_response.attributes = attributes
-      new_responses << question_response
+    survey.questions.each do |question|
+      attributes = question_response_params[question.id.to_s]
+      current_responses = response[question.id]
+      if question.question_type == 'checkbox' then
+        # We need to handle multiple responses.  The numerical response attributes hould be an
+        # array of option values (eg. indexes).
+        checked_options = attributes[:numerical_response]
+        
+        question.options.each_index do |index|
+          current_response = current_responses.find{|r| r.numerical_response == index} unless current_responses.nil?
+          checked = checked_options.include?(index.to_s)
+          p "For #{index}, checked is #{checked} and current_response is #{current_response.inspect}"
+          if checked && current_response.nil? then
+            p "#{index} is checked and current_response doesn't exist.  making new response."
+            # Need to make a new response
+            responses.build(:question => question, :numerical_response => index)
+          elsif !checked && !current_response.nil? then
+            p "#{index} is not checked and current response exists."
+            # Need to destroy the response as it is no longer checked.
+            current_response.destroy
+          end
+          
+          # Nothing is done when 'checked and current response not nil' and 'not checked and
+          # current response is nil') since everything is as it should be.
+        end
+        
+        if !attributes[:qualifications].blank? then
+          # assign the qualifications.  This is crufty.
+          sorted_responses = responses.sort {|r1, r2| r1.numerical_response <=> r2.numerical_response}
+          sorted_responses.first.qualifications = attributes[:qualifications]
+        end
+      else
+        if attributes.values.join.empty? then
+          # didn't respond
+          response[question.id].first.destroy if response[question.id] # So, destroy the previous response if it exists
+        else
+          question_response =  (current_responses.nil? || current_responses.empty?) ? responses.build(:question => question) : current_responses.first
+          question_response.attributes = attributes
+        end
+      end
     end
-    self.responses = new_responses
   end
   
   def response
@@ -43,4 +82,11 @@ class Participation < ActiveRecord::Base
       )
     end
   end
+  
+  def before_update
+    # we also want to update the associated records.  We'll assume it's valid by this point
+    # as we are validating the associated records.
+    responses.each { |r| r.save! if r.changed? }
+  end
+  
 end
