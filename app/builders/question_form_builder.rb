d4a7964052df5ac048b2b470c927b139d629ee15
Brian Terlson <brian.terlson@gmail.com>
2009-01-19 14:33:48 -0600

    Update form builder to utilize labels for radio buttons. Closes #151.

--- app/builders/question_form_builder.rb
+++ app/builders/question_form_builder.rb
@@ -9,9 +9,9 @@ class QuestionFormBuilder < ActionView::Helpers::FormBuilder
     when "numerical_field"
       label(:response, question.text) + text_field(:response, :size => 5) + error_text
     when "radio"
-      @template.content_tag(:label, question.text) +
+      @template.content_tag(:div, question.text, :class => "label") +
       question.options.to_enum(:each_with_index).collect { |option, index|
-        @template.content_tag(:div, radio_button(:response, index.to_f) + " " + option)
+        @template.content_tag(:label, radio_button(:response, index.to_f) + " " + option, :class => 'option')
       }.join("")
     when "text_area"
       label(:response, question.text) + text_area(:response, :rows => 5, :cols => 50)
