f75c346ff824184fa52faf11a641e471fc7f389b
Josh Fleck <flec0025@umn.edu>
2009-01-19 19:00:57 -0600

    Fixed feature I broke due to escaping issue

--- features/steps/account_steps.rb
+++ features/steps/account_steps.rb
@@ -10,10 +10,9 @@ Given /I am on the login page/ do
   visits new_session_url
 end
 
-Given /I am on the reset password page/ do
+Given /I am on the reset password page/ do  
   @current_organization = Organization.find_by_email("test@test.com")
-  visits reset_account_url(:key => CGI.escape(@current_organization.reset_password_key))
-  puts response.body
+  visits reset_account_url(:key => @current_organization.reset_password_key)
 end
 
 Given /there is an organization/ do
