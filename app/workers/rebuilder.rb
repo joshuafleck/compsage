71a76bf140f00ea71e364f14fcce8768d2f6a30c
Brian Terlson <brian.terlson@gmail.com>
2009-01-30 18:31:38 -0600

    Make the rebuilder work properly on the server, and configure god to insert the checker jobs when restarting beanstalkd.

--- app/workers/rebuilder.rb
+++ app/workers/rebuilder.rb
@@ -1,8 +1,11 @@
 require 'rake'
 # rebuilds the sphinx indexes at a specified interval
+Rake.application.add_import(RAILS_ROOT + "/Rakefile")
+Rake.application.load_imports
+
 class Rebuilder < BeanstalkWorker
   
-  def rebuild_sphinx_indexes(payload)
+  def rebuild_sphinx_indexes(payload)    
     Rake::Task["ts:index"].invoke
 
     # rebuild once a day
