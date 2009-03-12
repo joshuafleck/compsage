c55d71d4a13c899bcf2971b680e2f76ca580db8e
Josh Fleck <flec0025@umn.edu>
2009-03-11 22:02:38 -0500

    Switching all notifiers to use support as from address, Fixes #214

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
   # sent when an external network invitation is sent
   def external_network_invitation_notification(invitation)
     recipients invitation.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "#{invitation.inviter.name} invited you to join CompSage"
     body       :invitation => invitation, :network => invitation.network
@@ -12,7 +12,7 @@ class Notifier < ActionMailer::Base
   # sent when a global external invitation is sent
   def external_invitation_notification(invitation)
     recipients invitation.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "#{invitation.inviter.name} invited you to join CompSage"
     body       :invitation => invitation    
@@ -21,7 +21,7 @@ class Notifier < ActionMailer::Base
   # sent when an external survey invitation is sent
   def external_survey_invitation_notification(invitation)
     recipients invitation.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "#{invitation.inviter.name} invited you to participate in the survey \"#{invitation.survey.job_title}\""
     body       :invitation => invitation, :survey => invitation.survey  
@@ -30,7 +30,7 @@ class Notifier < ActionMailer::Base
   # Sent when a network invitation is sent
   def network_invitation_notification(invitation)
     recipients invitation.invitee.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "#{invitation.inviter.name} invited you to the network \"#{invitation.network.name}\""
     body       :invitation => invitation
@@ -39,7 +39,7 @@ class Notifier < ActionMailer::Base
   # Sent when a survey invitation is sent
   def survey_invitation_notification(invitation)
     recipients invitation.invitee.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "#{invitation.inviter.name} invited you to participate in the survey \"#{invitation.survey.job_title}\""
     body       :invitation => invitation, :survey => invitation.survey
@@ -47,7 +47,7 @@ class Notifier < ActionMailer::Base
   
   def survey_stalled_notification(survey)
     recipients survey.sponsor.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "Your survey \"#{survey.job_title}\" is stalled"
     body       :survey => survey
@@ -55,7 +55,7 @@ class Notifier < ActionMailer::Base
   
   def survey_results_available_notification(survey, participant)
     recipients participant.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "Survey results are available for \"#{survey.job_title}\""
     body       :survey => survey, :participant => participant
@@ -64,7 +64,7 @@ class Notifier < ActionMailer::Base
   #sent when a user requests a key for resetting their password
   def reset_password_key_notification(organization)
     recipients organization.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "You have requested a password reset"
     body       :organization => organization
@@ -73,7 +73,7 @@ class Notifier < ActionMailer::Base
   #sent when a survey sponsor posts a new discussion
   def discussion_thread_notification(discussion, recipient)
     recipients recipient.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "#{discussion.survey.sponsor.name} has responded to the discussion \"#{discussion.root.subject}\""
     body       :discussion => discussion, :recipient => recipient
@@ -82,7 +82,7 @@ class Notifier < ActionMailer::Base
   #sent when a survey invitee posts a new discussion
   def discussion_sponsor_notification(discussion)
     recipients discussion.survey.sponsor.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "A discussion has been posted for \"#{discussion.survey.job_title}\""
     body       :discussion => discussion
@@ -90,7 +90,7 @@ class Notifier < ActionMailer::Base
   
   def survey_rerun_notification_participant(survey,recipient)
     recipients recipient.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "The response deadline has been extended for the survey \"#{survey.job_title}\""
     body       :survey => survey, :recipient => recipient
@@ -98,7 +98,7 @@ class Notifier < ActionMailer::Base
   
   def survey_rerun_notification_non_participant(survey,recipient)
     recipients recipient.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "The response deadline has been extended for the survey \"#{survey.job_title}\""
     body       :survey => survey, :recipient => recipient
@@ -106,7 +106,7 @@ class Notifier < ActionMailer::Base
   
   def survey_not_rerunning_notification(survey,recipient)
     recipients recipient.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "The survey \"#{survey.job_title}\" did not receive enough responses"
     body       :survey => survey
@@ -114,7 +114,7 @@ class Notifier < ActionMailer::Base
   
   def new_organization_notification(organization)
     recipients organization.email
-    from       "CompSage <do-not-reply@compsage.com>"
+    from       "CompSage <support@compsage.com>"
     reply_to   "support@compsage.com"
     subject    "Your compsage.com account has been created"
     body       :organization => organization
--- config/initializers/exception_notification.rb
+++ config/initializers/exception_notification.rb
@@ -1,1 +1,3 @@
 ExceptionNotifier.exception_recipients = %w(eazydp@gmail.com brian.terlson@gmail.com flec0025@umn.edu)
+ExceptionNotifier.sender_address = %("Exception Notifier" <support@compsage.com>)
+
