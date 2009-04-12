5530a6a4c8fa836a4df5b19b47a9a91b71ca2f37
Brian Terlson <brian.terlson@gmail.com>
2009-04-12 01:09:16 -0500

    Qualifications without responses were being discarded instead of warning the user.

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -22,7 +22,7 @@ class Participation < ActiveRecord::Base
     survey.questions.each do |question|
       attributes = question_response_params[question.id.to_s] || {}
       current_responses = response[question.id]
-      if attributes[:response].blank? then
+      if attributes[:response].blank? && attributes[:qualifications].blank? then
         # didn't respond
         response[question.id].first.destroy if response[question.id] # So, destroy the previous response if it exists
       else
