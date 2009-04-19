5d50ff1ea4245b14e9838c11dc9c3d4308a1ce53
Brian Terlson <brian.terlson@gmail.com>
2009-04-19 11:58:48 -0500

    Initial stab at 2.3 compatibility, applying a workaround for count w/ scopes and has_many :through

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
-RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -15,6 +15,7 @@ Rails::Initializer.run do |config|
   config.gem 'thoughtbot-factory_girl', :lib => 'factory_girl', :source => 'http://gems.github.com'
   config.gem 'beanstalk-client', :version => '>=1.0.2'
   config.gem 'will_paginate', :version => '>=2.2.2'
+  config.gem 'webrat', :version => '>=0.4.4'
   config.gem 'activemerchant', :version => '>=1.4.0', :lib => 'active_merchant'  
   config.load_paths += ["app/workers", "app/builders"]
 end
