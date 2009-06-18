19322b01da02e6747f10e9de395e05b97c202aea
Brian Terlson <brian.terlson@gmail.com>
2009-06-17 21:13:36 -0500

    Use percentile calculation from NIST. Did you know there is no standard equation to calculate percentiles?

--- lib/statistics_extension.rb
+++ lib/statistics_extension.rb
@@ -1,25 +1,28 @@
 module StatisticsExtension
+  # Percentiles calculated based on NIST recommended algorithm.
+  # Returns nil if there is no data to calculate percentiles with.
   def percentiles(field, *percentiles)
     data = self.collect{|r| r.send(field) || 0 }
     return nil if data.empty?
     return Array.new(percentiles.size, data.first) if data.size == 1
+
     data.sort!
     size = data.size
     results = []
     
-    # To calculate the percentile, we first figure out exactly where in the range of numbers we fall.  We then take
-    # that position and find out what array index that corresponds to.  Next, we take the difference between the array
-    # position and the actual position to determine a weight by which we must adjust the percentile to get a more
-    # accurate representation of the data (assuming our chosen position isn't already the largest data element).
-   
     percentiles.each do |percentile|
-      pos = (size-1) * (percentile/100.0)
-      index = pos.to_i
-      weight = pos - index
-      
-      result = data[index]
-      result = result * (1-weight) + data[index + 1] * weight if index < size - 1
-      results << result
+      p  = percentile / 100.0
+      kd = p  * (size + 1)
+      k  = kd.to_i
+      d  = kd - k
+
+      if k == 0 then
+        results << data.first
+      elsif k >= size
+        results << data.last
+      else
+        results << data[k-1] + d * (data[k] - data[k-1])
+      end
     end
     
     results
