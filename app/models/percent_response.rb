29ae09b712373144ebdb830a60867b4f77f8fd18
Josh Fleck <flec0025@umn.edu>
2009-07-26 14:48:42 -0500

    Don't show percent sign when response is blank, Closes #366

--- app/models/percent_response.rb
+++ app/models/percent_response.rb
@@ -2,6 +2,6 @@ class PercentResponse < NumericalResponse
   self.field_options.merge!(:size => 4)
 
   def formatted_response
-    "#{number_with_delimiter(self.numerical_response)}%"
+    "#{number_with_delimiter(self.numerical_response)}%" unless self.numerical_response.blank?
   end
 end
