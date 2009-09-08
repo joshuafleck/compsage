9bfa1d940b9e0bda7049b5beb45a7a622fd3d046
David Peterson <dave@david-petersons-macbook-pro.local>
2009-09-08 18:09:58 -0500

    Removed hardcode from plug-in

--- vendor/plugins/asset_packager/lib/synthesis/asset_package_helper.rb
+++ vendor/plugins/asset_packager/lib/synthesis/asset_package_helper.rb
@@ -2,7 +2,7 @@ module Synthesis
   module AssetPackageHelper
     
     def should_merge?
-      AssetPackage.merge_environments.include?('production')#RAILS_ENV)
+      AssetPackage.merge_environments.include?(RAILS_ENV)
     end
 
     def javascript_include_merged(*sources)
