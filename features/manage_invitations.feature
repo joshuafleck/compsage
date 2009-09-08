71f1bdcf86f7f1dcc23c593bbefb87f3388bbed0
Brian Terlson <brian.terlson@gmail.com>
2009-09-07 20:58:39 -0500

    Use wait_for_ajax where necessary, and implement testing of sending invitations using the dropdown.

--- features/manage_invitations.feature
+++ features/manage_invitations.feature
@@ -7,3 +7,11 @@ Feature: Manage Invitations
     And I am on the survey invitations page
     When I create an external invitation
     Then I should be able to see the invitation
+
+  Scenario: Creating an internal survey invitation
+    Given I am testing javascript
+    And I am logged in
+    And I am sponsoring a "pending" survey
+    And I am on the survey invitations page
+    When I type in an existing organization and select it from the dropdown
+    Then I should be able to see the invitation
--- features/steps/invitation_steps.rb
+++ features/steps/invitation_steps.rb
@@ -2,9 +2,11 @@ Given /^I am on the survey invitations page$/ do
   goto(survey_invitations_url(@survey))
 end
 
-When "I type in an existing organization" do
+When "I type in an existing organization and select it from the dropdown" do
   create_organization("Existing Organization")
-  @browser.text_field(:id, 'external_invitation_organization_name').set_without_blur("Exis")
+  @browser.text_field(:id, 'external_invitation_organization_name').set_without_blur("Ex")
+  wait_for_ajax
+  @browser.div(:id, 'search_results').link(:href, 'javascript:;').click
 end
 
 When /^I create an external invitation$/ do
