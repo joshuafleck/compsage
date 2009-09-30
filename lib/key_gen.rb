692648e3a4085eaccd1b7731be70e4660ebe28bb
David Peterson <dave@david-petersons-macbook-pro.local>
2009-09-29 21:56:16 -0500

    Remove non-A# characters from the KeyGen lib because of errors cause when they were attached to the end of a URL. Regarding bug #410

--- lib/key_gen.rb
+++ lib/key_gen.rb
@@ -1,6 +1,6 @@
 module KeyGen
   # These characters are said to be url-safe as per RFC 3986
-  URL_SAFE_CHARS = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a + '-._~'.split(//)
+  URL_SAFE_CHARS = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
 
   # Construct a random url-safe key of the specified length.
   def self.random(length = 10)
