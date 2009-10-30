aa68e3471df77ab633794c709b6cafb66601f5e3
Brian Terlson <brian.terlson@gmail.com>
2009-10-29 20:05:53 -0500

    Beef up validation of contact form. Phone numbers are validated, and the email address and phone are only required when their respective contact preference is chosen.

--- app/models/contact_form_submission.rb
+++ app/models/contact_form_submission.rb
@@ -1,12 +1,18 @@
 class ContactFormSubmission
   include Validatable
-  attr_accessor :name, :email, :phone, :phone_extension, :contact_preference, :message
+  attr_accessor :name, :email, :phone, :phone_extension, :contact_preference, :message, :stripped_phone
 
   validates_presence_of :name, :message
 
-  validates_presence_of   :email
+  validates_presence_of   :email, :if => Proc.new { |c| c.contact_preference == 'email' }
   validates_format_of     :email, :with => Authentication::RE_EMAIL_OK, :message => Authentication::MSG_EMAIL_BAD,
     :if => Proc.new { |c| !c.email.blank? }
+
+  validates_presence_of   :phone, :if => Proc.new { |c| c.contact_preference == 'phone' }
+  # This validation is in the category of 'good enough'. It will accept any format so long as it includes 10 numbers
+  # with no more than two non-numeric characters in between the numbers. The phone number is allowed to be blank.
+  validates_format_of     :phone, :with => /^([^\d]?\d[^\d]?){10}$/, :if => Proc.new { |c| !c.phone.blank? }, 
+                          :message => "doesn't appear valid. Be sure to include your area code."
 
   def initialize(attributes = {})
     attributes ||= {}
