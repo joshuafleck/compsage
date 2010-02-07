276ca2612eea58bf52504735ea04d9d8573bc13d
Brian Terlson <brian.terlson@gmail.com>
2010-02-06 17:34:58 -0800

    Fix a bug in phone number formatter where the phone field was explicitly accessed and add a 'formatted_?' method to return the phone number in dasherized format.

--- lib/phone_number_formatter.rb
+++ lib/phone_number_formatter.rb
@@ -9,6 +9,16 @@ module PhoneNumberFormatter
       self.send :cattr_accessor, :phone_number_fields
       self.send :phone_number_fields=, attrs
       self.send :before_validation, :strip_phone_number_fields
+
+      attrs.each do |a|
+        define_method "formatted_#{a}" do
+          phone = self.send(a)
+
+          return nil if phone.nil?
+
+          return "#{phone[0, 3]}-#{phone[3, 3]}-#{phone[6, 4]}"
+        end
+      end
     end
   end
 
@@ -16,7 +26,7 @@ module PhoneNumberFormatter
     private
     def strip_phone_number_fields
       self.class.phone_number_fields.each do |field|
-        self.send(field).gsub!(/\D/, '') unless phone.blank?
+        self.send(field).gsub!(/\D/, '') unless self.send(field).blank?
       end
     end
   end
